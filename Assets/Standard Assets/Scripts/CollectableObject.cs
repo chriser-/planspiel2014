e4eab5585eb9b6cadaed138d8d90e6e7beacda2d
unknown <lorencic@GEPRAKTIKUM21.ad.rbg.tum.de>
2014-12-12 16:27:19 +0100

    michi & cic commit

--- Assets/Standard Assets/Scripts/CollectableObject.cs
+++ Assets/Standard Assets/Scripts/CollectableObject.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class CollectableObject : MonoBehaviour {
-
+    private Collider coll;
 	// Use this for initialization
 	void Start () {
 	
@@ -15,7 +15,26 @@ public class CollectableObject : MonoBehaviour {
 
     void OnTriggerEnter(Collider collider)
     {
+        coll = collider;
         Debug.Log("triggerenter: " + this.gameObject.name);
+        StartCoroutine(this.gameObject.name);
+        this.gameObject.renderer.enabled = false;
+        this.gameObject.collider.enabled = false;
+        //Destroy(this.gameObject);
+    }
+
+    void Banane() {
+        Debug.Log("Bananenfunktion");
+        //coll.gameObject.GetComponent<CharacterMotor>().jumping.enabled = false;
+    }
+
+    IEnumerator ColaDose() {
+        Debug.Log("Colafunktion");
+        coll.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 20.0f;
+        yield return new WaitForSeconds(10);
+
+        coll.gameObject.GetComponent<CharacterMotor>().movement.maxForwardSpeed = 10.0f;
         Destroy(this.gameObject);
     }
+
 }
