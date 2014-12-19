da61430ecf1967741666093b518ffda30e9a6284
unknown <lorencic@GEPRAKTIKUM16.ad.rbg.tum.de>
2014-12-19 14:33:47 +0100

    michi

---
+++ Assets/Standard Assets/Scripts/Zerstoerbar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zerstoerbar : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("triggerenter: " + this.gameObject.name);
+        
+        if (collider.gameObject.GetComponent<CharacterMotor>().movement.gravity > 50.0f)
+        {
+                Destroy(this.gameObject);
+        }
+        
+
+        //Destroy(this.gameObject);
+    }
+
+}
+
