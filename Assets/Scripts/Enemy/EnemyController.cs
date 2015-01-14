4d69a511f1d83dcc39fb4dca6db0f3968a1619c6
Christoph Winter <christoph.winter@local>
2015-01-14 21:24:07 +0100

    added PowerUpPanel prefab to make scene creation easier

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,35 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyBamgoo : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-
-    public Transform[] patrolWayPoints;
-    public float patrolSpeed = 2f;
-
-    private NavMeshAgent nav;
-    private int wayPointIndex = 0;
-
-    // Use this for initialization
     void Start()
     {
-        //nav = GetComponent<NavMeshAgent>();
+       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //nav.speed = patrolSpeed;
 
-        //Debug.Log(nav.remainingDistance + " " + nav.stoppingDistance);
-        //if (nav.remainingDistance <= nav.stoppingDistance)
-        //{
-        //    if (wayPointIndex == patrolWayPoints.Length - 1)
-        //        wayPointIndex = 0;
-        //    else
-        //        wayPointIndex++;
-        //}
-
-        //nav.destination = patrolWayPoints[wayPointIndex].position;
     }
 }
