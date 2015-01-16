1e05992fa74c21abcb82088d354a52f5ecc43c6a
Konfupanda <egor_sh@live.de>
2015-01-16 17:59:36 +0100

    fading of destroyable objects

--- Assets/Scripts/Object/DestroyableObject.cs
+++ Assets/Scripts/Object/DestroyableObject.cs
@@ -26,7 +26,28 @@ public class DestroyableObject : MonoBehaviour
                 child.gameObject.rigidbody.AddExplosionForce(20, position, 0, -1, ForceMode.VelocityChange);
             }
             destroyed = true;
+            StartCoroutine(MakeTransparent());
         }
+    }
+
+    IEnumerator MakeTransparent()
+    {
+        yield return new WaitForSeconds(2f);
+        foreach (Transform child in gameObject.transform) {
+            child.gameObject.renderer.material.shader = Shader.Find("Transparent/Diffuse");
+            Color c = child.gameObject.renderer.material.color;
+            child.gameObject.renderer.material.color = new Color(c.r, c.g, c.b, 1f);
+        }
+        for (int i = 0; i < 20; i++)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                Color c = child.gameObject.renderer.material.color;
+                child.gameObject.renderer.material.color = new Color(c.r, c.b, c.g, c.a - 0.05f);
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        Destroy(gameObject);
     }
 
 }
